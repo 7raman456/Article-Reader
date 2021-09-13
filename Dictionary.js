7c1ab275af73ea62296d8aa480ef9d91aa377ee5
RamanDeep Singh <44069082+7raman456@users.noreply.github.com>
2021-09-13 16:55:29 +0530

    added some comments

--- Dictionary.js
+++ Dictionary.js
@@ -6,7 +6,53 @@ jumbobutton.onclick=function(){
     formsubmit.style.display="block";
     welcome.style.display="none";
 }
-   function mean(){
+ 
+
+// funtionality begin
+
+
+function myFunction() { 
+	document.getElementById("mainFrameOne").style.display="none"; 
+    var article_heading=document.getElementById("exampleFormControlInput1").value;
+    
+    //console.log(article_heading);
+    var article_content=document.getElementById("exampleFormControlTextarea1").value;
+    //console.log(article_content);
+    myFunctionDisplay();
+    mean();
+
+    
+
+}
+
+
+//////////////text converted to search meanings raw
+function convert_to_text(str) {
+    var result=str.split("\n");
+    
+    //console.log(res);
+    str="";
+    for(var j=0;j<result.length;j++)
+    {
+        var res = result[j].split(" ");
+        for(var i=0;i<res.length;i++)
+    {
+        str+="<span>"+res[i]+"</span>"+" "
+        
+    }
+    str+="<br>";
+    }
+    
+    return str;
+    
+  }
+
+
+
+
+// added functions to all words to click and search meaning
+
+  function mean(){
    var x=document.getElementsByTagName("span");
 for(var i=0;i<x.length;i++)
 {
@@ -27,55 +73,24 @@ for(var i=0;i<x.length;i++)
 }
 }
    }
-function myFunction3(str) {
-    var result=str.split("\n");
-    
-    //console.log(res);
-    str="";
-    for(var j=0;j<result.length;j++)
-    {
-        var res = result[j].split(" ");
-        for(var i=0;i<res.length;i++)
-    {
-        str+="<span>"+res[i]+"</span>"+" "
-        
-    }
-    str+="<br>";
-    }
-    
-    return str;
-    
-  }
 
-function myFunction() { 
-	document.getElementById("mainFrameOne").style.display="none"; 
-    var article_heading=document.getElementById("exampleFormControlInput1").value;
-    
-    //console.log(article_heading);
-    var article_content=document.getElementById("exampleFormControlTextarea1").value;
-    //console.log(article_content);
-    myFunctionDisplay();
-    mean();
 
-    
-
-}
-
+/////////////////// displaying in order to be readable.
 function myFunctionDisplay() { 
     //document.getElementById("mainFrameOne").style.display="none"; 
     
     var article_heading=document.getElementById("exampleFormControlInput1").value;
-    article_heading=myFunction3(article_heading);
+    article_heading=convert_to_text(article_heading);
     document.getElementById("headinggg").innerHTML=article_heading;
     //console.log(article_heading);
     var article_content=document.getElementById("exampleFormControlTextarea1").value;
-    article_content=myFunction3(article_content);
+    article_content=convert_to_text(article_content);
     document.getElementById("content").innerHTML=article_content;
     //console.log(article_content);
 
 }
 
-
+//////////////fucntion that helps to ammend meanings in html page
 function pushmeaning(word,wordmeaning) { 
  var meanings=document.getElementById("meaning").innerHTML;
  word="<h6>"+word+"</h6>" 
